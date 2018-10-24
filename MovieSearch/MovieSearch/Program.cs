678fe4ecab76969f59961b60347c357d29f8de33
Oliver Gray <oligray@hotmail.com>
2018-10-24 17:09:41 -0400

    Initial Test Project created

---
+++ MovieSearch/MovieSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Movie Title to Search for: ");
+
+            string searchTerm = Console.ReadLine();
+
+            string[] results = getMovieTitles(searchTerm);
+
+            //Output movie titles list to the Console:
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static string[] getMovieTitles(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
